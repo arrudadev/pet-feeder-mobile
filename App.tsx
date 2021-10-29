819424f63ca23b6b191c5105e046c0685f4a0471
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-10-29 20:58:31 -0300

    chore: add font jost from google fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
+
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from '@expo-google-fonts/jost';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,6 +20,13 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
