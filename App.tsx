2e3ebd1beb523a9a741a6ff392244f7c51a2632f
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-10-29 20:40:12 -0300

    chore: setup eslint

--- App.tsx
+++ App.tsx
@@ -1,15 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StatusBar } from 'expo-status-bar';
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +11,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar />
+    </View>
+  );
+}
+
